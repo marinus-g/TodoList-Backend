9059bb309a36afbf0a748f6ae5aef58c019e8719
marinus-g <marinusmail@protonmail.com>
2024-05-28 08:37:14 +0200

    Initial Commit

---
+++ src/test/java/academy/mischok/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.mischok.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
