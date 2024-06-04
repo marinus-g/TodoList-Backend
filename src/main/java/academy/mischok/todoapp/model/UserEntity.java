41524c8b49912ff3076da4a1764cde56cf39aee4
marinus-g <marinusmail@protonmail.com>
2024-06-04 10:22:44 +0200

    fixed user authentication

--- src/main/java/academy/mischok/todoapp/model/UserEntity.java
+++ src/main/java/academy/mischok/todoapp/model/UserEntity.java
@@ -1,6 +1,10 @@
 package academy.mischok.todoapp.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
