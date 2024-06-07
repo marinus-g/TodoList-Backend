d76d7bb774863627d7a9d979cff0ca917f8072b8
BorisLevant <Boris.Levant@tn.mischok.academy>
2024-06-07 13:23:37 +0200

    StudyPlan first tests

--- src/test/java/academy/mischok/todoapp/controller/StudyPlanControllerTest.java
+++ src/test/java/academy/mischok/todoapp/controller/StudyPlanControllerTest.java
@@ -15,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class StudyPlanControllerTest extends AuthenticatedBaseControllerTest {
 
     @Test
-    void testCreateStudyPlan() throws Exception {
+    void testCreateValidStudyPlan() throws Exception {
 
         final ObjectMapper objectMapper = new ObjectMapper();
         String contentAsString = mockMvc.perform(get("/studyplan")
@@ -42,5 +42,112 @@ public class StudyPlanControllerTest extends AuthenticatedBaseControllerTest {
                 .andExpect(header().string("Location", containsString("/studyplan/")));
         mockMvc.perform(get("/studyplan").cookie(super.defaultCookie))
                 .andExpect(jsonPath("$", hasSize(length + 1)));
+    }
+
+    @Test
+    void createdStudyPlanWithShortTitle() throws Exception {
+        final String data = """
+                {
+                    "title": "T",
+                    "start_date": "20-04-2024",
+                    "end_date": "24-04-2024",
+                }
+                """;
+
+        mockMvc.perform(post("/studyplan")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(data)
+                        .cookie(super.defaultCookie)
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("Title too short"));
+
+    }
+
+    @Test
+    void createdStudyPlanWithBlankTitle() throws Exception {
+        final String data = """
+                {
+                    "title": "",
+                    "start_date": "20-04-2024",
+                    "end_date": "24-04-2024",
+                }
+                """;
+
+        mockMvc.perform(post("/studyplan")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(data)
+                        .cookie(super.defaultCookie)
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("Title should not be empty"));
+
+    }
+
+    @Test
+    void createdStudyPlanWithNoTitle() throws Exception {
+        final String data = """
+                {
+                    "start_date": "20-04-2024",
+                    "end_date": "24-04-2024",
+                }
+                """;
+
+        mockMvc.perform(post("/studyplan")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(data)
+                        .cookie(super.defaultCookie)
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("Title should not be empty"));
+
+    }
+
+    @Test
+    void createdStudyPlanWithInvalidStartDate() throws Exception {
+        final String data = """
+                {
+                    "title": "test",
+                    "start_date": "15-2020",
+                    "end_date": "12-12-2020",
+                }
+                """;
+
+        mockMvc.perform(post("/studyplan")
+                        .cookie(super.defaultCookie)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(data)
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("Invalid Date"));
+
+    }
+
+    @Test
+    void createdStudyPlanWithInvalidEndDate() throws Exception {
+        final String data = """
+                {
+                    "title": "test",
+                    "start_date": "15-12-2020",
+                    "end_date": "12-2020",
+                }
+                """;
+
+        mockMvc.perform(post("/studyplan")
+                        .cookie(super.defaultCookie)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(data)
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("Invalid Date"));
+
+    }
+
+    //@Test
+    void createdStudyPlanWithNoStartDate() throws Exception {
+    }
+
+    //@Test
+    void createdStudyPlanWithNoEndDate() throws Exception {
     }
 }
